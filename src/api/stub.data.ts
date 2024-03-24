859f8be6e7b015d360689b118eaedccbd4854d71
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-03-24 13:31:57 -0400

    more links

--- src/api/stub.data.ts
+++ src/api/stub.data.ts
@@ -14,14 +14,20 @@ export const stub: DashboardGroup[] = [
             {
                 id: 2,
                 text: "Proton",
-                description: "email",
+                description: "Better webmail",
                 href: "https://mail.proton.me",
             },
             {
                 id: 3,
                 text: "GitHub",
-                description: "making eyes bleed",
+                description: "Making eyes bleed",
                 href: "https://github.com",
+            },
+            {
+                id: 4,
+                text: "Notion",
+                description: "Note database",
+                href: "https://notion.so",
             }
         ]
     },
@@ -34,6 +40,48 @@ export const stub: DashboardGroup[] = [
                 text: "Hacker News",
                 description: "half decent news",
                 href: "https://news.ycombinator.com/",
+            },
+            {
+                id: 202,
+                text: "Edx",
+                description: "Icky corp that replaced opencourseware",
+                href: "https://www.edx.org/",
+            },
+            {
+                id: 202,
+                text: "Current Events",
+                description: "Whats going on in the world",
+                href: "https://www.imdb.com/title/tt0387808/",
+            }
+        ]
+    },
+    {
+        id: 3,
+        text: "Nifty Projects",
+        links: [
+            {
+                id: 301,
+                text: "Leaflet Js",
+                description: "Openstreetmap based interactive map",
+                href: "https://leafletjs.com/",
+            },
+            {
+                id: 302,
+                text: "Sqids",
+                description: "Reversible number hasher",
+                href: "https://sqids.org/",
+            },
+            {
+                id: 303,
+                text: "World time buddy",
+                description: "World time chart",
+                href: "https://www.worldtimebuddy.com/",
+            },
+            {
+                id: 303,
+                text: "Strudel",
+                description: "Coding is an instrument",
+                href: "https://strudel.cc/",
             }
         ]
     }
