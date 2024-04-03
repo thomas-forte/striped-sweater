ee36e03faa841cd276a32f344d171a21bfa13cc0
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-04-02 21:07:30 -0400

    leedle api update

--- src/api/leedle.api.ts
+++ src/api/leedle.api.ts
@@ -3,11 +3,19 @@ import { GhPrs } from "./leedle.types";
 import { formatTimeAgo } from "../common/formatTime";
 
 const leedleClient = axios.create({
-  baseURL: "http://silent.but.deadly.farts/api",
+  baseURL: "http://silent.but.deadly.farts/",
 });
 
+export const isLeedleInReach = async (): Promise<boolean> => {
+  const response = await leedleClient
+    .get("ping")
+    .catch(() => ({ status: false }));
+
+  return response.status === 200;
+};
+
 export const getAssignedPullRequests = async (): Promise<GhPrs> => {
-  const response = (await leedleClient.get("gh-assigned")).data;
+  const response = (await leedleClient.get("api/gh-assigned")).data;
 
   if (response.pull_requests && Array.isArray(response.pull_requests)) {
     response.pull_requests.forEach((element: any) => {
