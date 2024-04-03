aff52f46cb773e9d61574439642a90abc508b9ee
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-04-02 20:41:11 -0400

    simple time formatting

---
+++ src/common/formatTime.tsx
@@ -0,0 +1,25 @@
+const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
+
+type TimeDivision = { amount: number; unit: Intl.RelativeTimeFormatUnit };
+
+const DIVISIONS: TimeDivision[] = [
+  { amount: 60, unit: "seconds" },
+  { amount: 60, unit: "minutes" },
+  { amount: 24, unit: "hours" },
+  { amount: 7, unit: "days" },
+  { amount: 4.34524, unit: "weeks" },
+  { amount: 12, unit: "months" },
+  { amount: Number.POSITIVE_INFINITY, unit: "years" },
+];
+
+export const formatTimeAgo = (date: Date) => {
+  let duration = (date.getTime() - new Date().getTime()) / 1000;
+
+  for (let i = 0; i < DIVISIONS.length; i++) {
+    const division = DIVISIONS[i];
+    if (Math.abs(duration) < division.amount) {
+      return rtf.format(Math.round(duration), division.unit);
+    }
+    duration /= division.amount;
+  }
+};
